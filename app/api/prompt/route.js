dc101387c7a3d954c289586fb211ce0df242a21f
OMEN <ayxannematov96@gmail.com>
2023-10-30 11:07:23 +0400

    Feed page was rechecked

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ import Prompt from "@/models/prompt";
 export const GET = async (request) => {
   try {
     await connectDB();
-    const prompts = await Prompt.find({}).populate("creator");
+    const prompts = await Prompt.find().populate("creator");
     return NextResponse.json(prompts, { status: 200 });
   } catch (error) {
     return new NextResponse("Failed to fetch prompts", { status: 500 });
